239125dcc77ec22e98838a6dfbca0f3b9e02bac7
dudleyneedham <dudleyneedham1993@gmail.com>
2021-08-23 17:50:43 +0200

    feat: adding styling information

--- src/container/IdentityView/IdentityView.tsx
+++ src/container/IdentityView/IdentityView.tsx
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react'
 import { Button } from '../../components/Button/Button'
 import { StateContext } from '../../utils/StateContext'
-import styles from './IdentityView.module.css'
 import { shortenAddress } from '../../utils/shortenAddress'
 import { TokenBar } from '../../components/Dashboard/TokenBar'
 import { Identicon } from '../../components/Identicon/Identicon'
+import styles from './IdentityView.module.css'
+import cx from 'classnames'
+
+function getPercent(percentageValue: number, secondValue: number) {
+  const total = percentageValue + secondValue
+  const percent = (percentageValue / total) * 100
+  return percent.toFixed(1)
+}
 
 export interface Props {
   toggleDetailedIdentityView: () => void
@@ -20,20 +27,71 @@ export const IdentityView: React.FC<Props> = ({
 
   // placeholder for the error notifications
   if (!account) return <></>
-
-  const shortAddress = shortenAddress(account.address)
+  const shortCollatorAddress = shortenAddress(account.address)
 
   return (
     <div className={styles.identityView}>
       <div className={styles.container}>
-        <Identicon address={account.address} />
-        <>
-          {account?.name}
-          <span />
-          {shortAddress}
-        </>
-        <TokenBar staked={account.staked} stakeable={account.stakeable} />
-        <p>MY STAKE {account.staked}</p>
+        <div className={styles.identityViewHeader}>
+          <div className={styles.identiconContainer}>
+            <Identicon address={account.address} />
+          </div>
+          <div className={cx(styles.label, styles.labelGray, styles.name)}>
+            {account?.name}
+          </div>
+          <div className={styles.tokenbarContainer}>
+            <TokenBar staked={account.staked} stakeable={account.stakeable} />
+          </div>
+        </div>
+
+        <div className={styles.identityStakeContainer}>
+          <span className={cx(styles.labelSmall, styles.labelGray)}>
+            my stake <br />
+            <span className={cx(styles.label, styles.labelYellow)}>
+              {account.staked.toLocaleString()} KILT | <span />
+              {getPercent(account.staked, account.stakeable)} %
+            </span>
+          </span>
+
+          <span
+            className={cx(
+              styles.labelSmall,
+              styles.labelGray,
+              styles.textRight
+            )}
+          >
+            stakeable <br />
+            <span className={cx(styles.label, styles.labelOrange)}>
+              {getPercent(account.stakeable, account.staked)} % | <span />
+              {account.stakeable.toLocaleString()} KILT
+            </span>
+          </span>
+        </div>
+
+        <div className={styles.identityViewDetailsContainer}>
+          <>
+            STAKE COLLATOR <br />
+            {shortCollatorAddress}
+            since AUG 2021 <br />
+          </>
+          <>
+            RANK | TOTAL STAKE <br />
+            888 88,888,888.88 KILT
+            <br />
+          </>
+          <>
+            LOWEST STAKE | DELEGATORS <br />
+            8,888.88 KILT 88/25
+          </>
+          <>
+            REWARD (%) / YEAR <br />
+            8.88 %
+          </>
+          <>
+            PREDICTED REWARD <br />
+            888,888,888.88 KILT
+          </>
+        </div>
       </div>
       <div className={styles.buttonContainer}>
         <Button
