eb518bddcfd974df970878e3717205cf0b4a027d
Timo Welde <tjwelde@gmail.com>
2021-09-01 15:48:09 +0200

    fix: used accounts might just have unstaking funds

--- src/container/IdentitySelector/IdentitySelector.tsx
+++ src/container/IdentitySelector/IdentitySelector.tsx
@@ -10,7 +10,7 @@ export interface Props {
 
 export const IdentitySelector: React.FC<Props> = ({ onChange, accounts }) => {
   const options = accounts
-    .filter((account) => !account.used)
+    .filter((account) => account.staked === 0)
     .map((account) => ({
       value: account.address,
       label: `${account.name} (Stakeable: ${format(account.stakeable)})`,
