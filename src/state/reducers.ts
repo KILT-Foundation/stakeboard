7f1e97d767078afce377480d30b0a3c8f841ad5b
Timo Welde <tjwelde@gmail.com>
2021-09-10 16:49:57 +0200

    refactor: move stored reducers into separate file

--- src/state/reducers.ts
+++ src/state/reducers.ts
@@ -1,24 +1,6 @@
 import { Reducer } from 'react'
 import { Account } from '../types'
 
-export type FavoriteActions =
-  | { type: 'favorize'; id: string }
-  | { type: 'unfavorize'; id: string }
-
-export const favoriteReducer: Reducer<string[], FavoriteActions> = (
-  state,
-  action
-) => {
-  switch (action.type) {
-    case 'favorize':
-      return [...state, action.id]
-    case 'unfavorize':
-      return state.filter((id) => id !== action.id)
-    default:
-      return state
-  }
-}
-
 export type PausedAction = { type: 'refreshPaused'; refreshPaused: boolean }
 
 export type ConnectionActions =
