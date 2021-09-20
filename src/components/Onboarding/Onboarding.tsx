f332c6342481b21f8a864f521076842801d679a6
Timo Welde <tjwelde@gmail.com>
2021-09-20 19:20:47 +0200

    feat: add a termsAccepted state

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -34,7 +34,9 @@ export const OnboardingBg: React.FC<Props> = ({
         }}
         className={styles.backgroundImage}
       >
-        {children}
+        {
+          // TODO: Show different screens depending on status
+        }
       </div>
     )
   } else {
