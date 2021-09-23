a439f64a7052b41656eaf3d49fa84af36b9f9051
dudleyneedham <dudleyneedham1993@gmail.com>
2021-09-23 13:59:28 +0200

    refactor: switch conditional percentage

--- src/components/Dashboard/TokenBar.tsx
+++ src/components/Dashboard/TokenBar.tsx
@@ -17,7 +17,7 @@ const BarItem: React.FC<BarItemProps> = ({ style, amount, percentage }) => {
   return (
     <div
       className={cx(styles.item, style)}
-      style={{ width: !percentage ? barWidth : percentage + '%' }}
+      style={{ width: percentage ? percentage + '%' : barWidth }}
     ></div>
   )
 }
@@ -53,7 +53,7 @@ export const TokenBar: React.FC<TokenBarProps> = ({
   return (
     <div
       className={cx(styles.container, { [styles.containerDown]: down })}
-      style={{ width: !percentage ? container_width : '98%' }}
+      style={{ width: percentage ? '100%' : container_width }}
     >
       {has_staked && (
         <BarItem
