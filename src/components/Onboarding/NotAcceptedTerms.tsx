69e9bd2434913ad3a8312b85a9be5030f213a31e
dudleyneedham <dudleyneedham1993@gmail.com>
2021-09-30 14:30:45 +0200

    feat: added checkbox for agreeing on terms and wording changed

--- src/components/Onboarding/NotAcceptedTerms.tsx
+++ src/components/Onboarding/NotAcceptedTerms.tsx
@@ -1,11 +1,12 @@
 import styles from './Onboarding.module.css'
 import { Button } from '../Button/Button'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { StoredStateContext } from '../../utils/StoredStateContext'
-import Pdf from '../../uploads/210930_Stakeboard_Terms&License.pdf'
+import PDF from '../../uploads/210930_Stakeboard_Terms&License.pdf'
 
 export const NotAcceptedTerms: React.FC = () => {
   const { dispatch } = useContext(StoredStateContext)
+  const [acceptedTerms, setAcceptedTerms] = useState<boolean>(true)
   return (
     <>
       <span className={styles.highlighted}>Hey Staker,</span>
@@ -17,23 +18,24 @@ export const NotAcceptedTerms: React.FC = () => {
         Sleep less, stake more!
       </p>
       <p className={styles.text}>
-        In order to enter the halfpipe, please make sure to agree to the Terms
-        &amp; Conditions.
-      </p>
-      <p className={styles.text}>
-        Read Terms:
+        <input
+          type={'checkbox'}
+          onClick={() => setAcceptedTerms(!acceptedTerms)}
+        />
+        I have read and agree to the
         <a
-          href={Pdf}
+          href={PDF}
           rel="noopener noreferrer"
           target="_blank"
           className={styles.termsLink}
         >
-          Terms and Conditions
+          Terms of Use for the KILT Stakeboard Web Application
         </a>
       </p>
       <span className={styles.highlighted}>
         <Button
-          label="Accept"
+          label="continue"
+          disabled={acceptedTerms}
           onClick={() => {
             dispatch({ type: 'acceptTerms' })
           }}
