77efdb0512c60bdfe9f62e138319a3686d2e3d1f
dudleyneedham <dudleyneedham1993@gmail.com>
2021-10-12 14:44:26 +0200

    feat: adding staking percentage and caps

--- src/utils/stakePercentage.ts
+++ src/utils/stakePercentage.ts
@@ -1,3 +1,5 @@
+import { BlockNumber } from '@polkadot/types/interfaces'
+
 export function getPercent(percentageValue: number, secondValue: number) {
   const total = percentageValue + secondValue
   const percent = (percentageValue / total) * 100
@@ -6,4 +8,20 @@ export function getPercent(percentageValue: number, secondValue: number) {
 
 export function getPercentage(total: number, value: number) {
   return ((total / value) * 100).toFixed(1)
+}
+
+export function delegatorsRewardRate(
+  currentStakeRate: number,
+  blocks: BlockNumber
+) {
+  const blocksInYear = 31557600 / 12
+  let rewardRate
+  if (blocks.toNumber() <= blocksInYear) {
+    rewardRate = Math.min(40 / currentStakeRate, 1) * 8
+  } else if (blocks.toNumber() <= blocksInYear * 2) {
+    rewardRate = Math.min(40 / currentStakeRate, 1) * 6
+  } else {
+    rewardRate = 0
+  }
+  return rewardRate.toFixed(1)
 }
