c2f771017c334766d905ce58b105423e8b961d36
dudleyneedham <dudleyneedham1993@gmail.com>
2021-10-13 17:36:23 +0200

    feat: making the delegators reward rate more verbose

--- src/utils/stakePercentage.ts
+++ src/utils/stakePercentage.ts
@@ -1,5 +1,3 @@
-import { BlockNumber } from '@polkadot/types/interfaces'
-
 export function getPercent(percentageValue: number, secondValue: number) {
   const total = percentageValue + secondValue
   const percent = (percentageValue / total) * 100
@@ -10,15 +8,16 @@ export function getPercentage(total: number, value: number) {
   return ((total / value) * 100).toFixed(1)
 }
 
-export function delegatorsRewardRate(
-  currentStakeRate: number,
-  blocks: BlockNumber
-) {
-  const blocksInYear = 31557600 / 12
+export function delegatorsRewardRate(currentStakeRate: number, blocks: number) {
+  const minuteInSeconds = 60
+  const hourInMinutes = minuteInSeconds * 60
+  const dayInHours = hourInMinutes * 24
+  const yearInDays = dayInHours * 365.25
+  const blocksInYear = yearInDays / 12
   let rewardRate
-  if (blocks.toNumber() <= blocksInYear) {
+  if (blocks <= blocksInYear) {
     rewardRate = Math.min(40 / currentStakeRate, 1) * 8
-  } else if (blocks.toNumber() <= blocksInYear * 2) {
+  } else if (blocks <= blocksInYear * 2) {
     rewardRate = Math.min(40 / currentStakeRate, 1) * 6
   } else {
     rewardRate = 0
