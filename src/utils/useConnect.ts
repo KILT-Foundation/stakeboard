77aae7f16022c4fc9780004ce5dfcd541f7974e3
Raphael Flechtner <raphael@kilt.io>
2022-12-08 16:24:57 +0100

    feat: allow randomizing priority of endpoints

--- src/utils/useConnect.ts
+++ src/utils/useConnect.ts
@@ -12,6 +12,14 @@ const ENDPOINT =
   process.env.REACT_APP_FULL_NODE_ENDPOINT?.split(',').map((i) => i.trim()) ||
   'wss://peregrine.kilt.io/parachain-public-ws'
 
+if (ENDPOINT instanceof Array && process.env.REACT_APP_SHUFFLE_ENDPOINTS) {
+  // shuffle endpoint priority
+  for (let i = ENDPOINT.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[ENDPOINT[i], ENDPOINT[j]] = [ENDPOINT[j], ENDPOINT[i]]
+  }
+}
+
 export const useConnect = () => {
   const { dispatch } = useContext(StateContext)
 
