d3f17bf6cc64b18785f07c072ab8059b1c18662c
Raphael Flechtner <raphael@kilt.io>
2022-12-09 11:09:16 +0100

    chore: put connect in separate function

--- src/utils/useConnect.ts
+++ src/utils/useConnect.ts
@@ -12,25 +12,35 @@ import jsonParachainSpec from './spiritnet.json'
 let cachedApi: Promise<ApiPromise> | null = null
 let provider: ScProvider | null = null
 
+async function createLightClientApi(onProvider: (p: ScProvider) => void) {
+  // Create the provider for the relay chain
+  const polkadotProvider = new ScProvider(Sc, Sc.WellKnownChain.polkadot)
+  // Create the provider for the parachain. Notice that
+  // we must pass the provider of the relay chain as the
+  // second argument
+  const parachainSpec = JSON.stringify(jsonParachainSpec)
+  provider = new ScProvider(Sc, parachainSpec, polkadotProvider)
+  // call onProvider
+  onProvider(provider)
+  // establish the connection (and catch possible errors)
+  await provider.connect()
+  // Create the PolkadotJS api instance
+  return ApiPromise.create({
+    provider,
+    typesBundle,
+    noInitWarn: process.env.NODE_ENV === 'production',
+  })
+}
+
 export const useConnect = () => {
   const { dispatch } = useContext(StateContext)
 
   if (!cachedApi) {
-    const parachainSpec = JSON.stringify(jsonParachainSpec)
-
-    const polkadotProvider = new ScProvider(Sc, Sc.WellKnownChain.polkadot)
-    provider = new ScProvider(Sc, parachainSpec, polkadotProvider)
-
-    provider.connect()
-    cachedApi = ApiPromise.create({
-      provider,
-      typesBundle,
-      noInitWarn: process.env.NODE_ENV === 'production',
+    cachedApi = createLightClientApi((provider) => {
+      provider.on('disconnected', () => dispatch({ type: 'disconnected' }))
+      provider.on('connected', () => dispatch({ type: 'connected' }))
+      provider.on('error', (error) => dispatch({ type: 'error', err: error }))
     })
-
-    provider.on('disconnected', () => dispatch({ type: 'disconnected' }))
-    provider.on('connected', () => dispatch({ type: 'connected' }))
-    provider.on('error', (error) => dispatch({ type: 'error', err: error }))
   }
 
   return cachedApi
