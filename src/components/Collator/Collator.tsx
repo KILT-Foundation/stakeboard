1f8649e2f955d6b6ba820322aefdbcae4b0145f8
Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>
2023-03-16 18:06:48 +0100

    fix: compatibility with did runtime api V2 (#90)
    * fix: compatibility with did runtime api V2
    * chore: bump kilt type definitions & augmentation

--- src/components/Collator/Collator.tsx
+++ src/components/Collator/Collator.tsx
@@ -16,7 +16,15 @@ export const Collator: React.FC<Props> = ({ address }) => {
   useEffect(() => {
     const getWeb3name = async () => {
       const api = await getConnection()
-      const connectedDid = await api.call.did.queryByAccount(address)
+      // figure out which did runtime api version we are dealing with
+      const { sectionHash } = api.call.did.queryByAccount.meta
+      const apiVersion = api.runtimeVersion.apis
+        .find(([hash]) => hash.eq(sectionHash))![1]
+        .toNumber()
+      // api v2 expects a different parameter format than api v1
+      const connectedDid = await api.call.did.queryByAccount(
+        apiVersion >= 2 ? { AccountId32: address } : address
+      )
       if (connectedDid.isSome) {
         const web3name = connectedDid.unwrap().w3n
         if (web3name.isSome) {
